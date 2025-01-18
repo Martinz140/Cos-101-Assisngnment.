bf86cd204b5101fd000a1541252283d31b6126bc
Martinz140 <martinsisrael140@gmail.com>
2025-01-18 14:01:57 +0100

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+
+from tkinter import Tk, Entry, Button, Label, StringVar
+window=Tk()
+window.geometry("600x250")
+window.title("Multi-lingual Dictionary")
+
+entry_text = Entry()
+entry_text.pack()
+
+result = StringVar
+result_label = (window, text_variable == result)
+result_label.pack()
+
+
+window.mainloop()
